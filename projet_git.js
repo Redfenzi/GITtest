01463d899d74e5ef5e07c8db8a7cf9b4750f3214
redouane.fenzi <redouane.fenzi@depinfoad.univ-brest.fr>
2023-01-11 08:46:56 +0100

    AJOUT DE REBASE AUSSI DANS L4HISTOIR Rebase

--- projet_git.js
+++ projet_git.js
@@ -7,6 +7,7 @@ const operand2 = 45;
 let result = operand + operand2;
 
 console.log("resultat de" + operand + "+" + operand2 + "=" + result);
+<<<<<<< HEAD
 =======
 b+=a;
 console.log("resultat de a + b = "+ b);
@@ -21,4 +22,35 @@ let aPerson = {
 aPerson.sayHello = function() {console.log('Bonjour ! Je suis ' + this.name + '.'); };
 
 aPerson.sayHello();
+<<<<<<< HEAD
 >>>>>>> a6779d7 (AFFICHAGE D'UN MESSAGE Bonjour ! Je suis ...)
+=======
+>>>>>>> b1
+=======
+
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Dimanche";
+    break;
+  case 1:
+    day = "Lundi";
+    break;
+  case 2:
+     day = "Mardi";
+    break;
+  case 3:
+    day = "Mercredi";
+    break;
+  case 4:
+    day = "Jeudi";
+    break;
+  case 5:
+    day = "Vendredi";
+    break;
+  case 6:
+    day = "Samedi";
+}
+console.log("Aujourd'hui nous sommes " + day);
+>>>>>>> bad5a82 (Ajout d'une classe Day)
+>>>>>>> 18da9d6 (Rebase)
