ae0226a7825955fbaf1dd54e2a1730d38be05b8f
unknown <redouane.fenzi@depinfoad.univ-brest.fr>
2023-01-11 08:29:59 +0100

    AFFICHAGE D'UN MESSAGE Bonjour ! Je suis ...
    CODE PLUS EXPLICITE
    - ajout d'une classe aPerson
    - Création de la fonction

--- projet_git.js
+++ projet_git.js
@@ -3,6 +3,22 @@ console.log('Hello, world!')
 const operand = 12;
 const operand2 = 45;
 
+<<<<<<< HEAD
 let result = operand + operand2;
 
 console.log("resultat de" + operand + "+" + operand2 + "=" + result);
+=======
+b+=a;
+console.log("resultat de a + b = "+ b);
+
+let aPerson = { 
+	name: 'Jean',
+	age: 35,
+	gender: 'masculin',
+	interest: ['musique', 'Jeux videos'],
+};
+
+aPerson.sayHello = function() {console.log('Bonjour ! Je suis ' + this.name + '.'); };
+
+aPerson.sayHello();
+>>>>>>> a6779d7 (AFFICHAGE D'UN MESSAGE Bonjour ! Je suis ...)
